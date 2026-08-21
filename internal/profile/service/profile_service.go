63930a3149404801c1e3f11ecd55e9f63750f7de
Mosteben <alaa16210@gmail.com>
2026-08-22 00:07:30 +0300

    feat: add profile service

---
+++ internal/profile/service/profile_service.go
@@ -0,0 +1,9 @@
+package service
+
+// ProfileService is a placeholder service for profile operations.
+type ProfileService struct{}
+
+// NewProfileService creates a new ProfileService instance.
+func NewProfileService() *ProfileService {
+	return &ProfileService{}
+}
